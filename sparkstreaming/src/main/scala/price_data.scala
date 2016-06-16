43a3230c2525fb6373ff04c194c21841c65a8c71
Ubuntu <ubuntu@ip-172-31-1-71.us-west-2.compute.internal>
2016-06-16 20:41:52 +0000

    adding min max separate tuple

--- sparkstreaming/src/main/scala/price_data.scala
+++ sparkstreaming/src/main/scala/price_data.scala
@@ -16,7 +16,7 @@ import com.datastax.driver.core.utils._
 object JSONDataStreaming {
   def main(args: Array[String]) {
 
-    val brokers = "52.33.31.202:9092"
+    val brokers = "52.39.57.55:9092,52.40.218.111:9092,52.24.38.215:9092,52.33.31.202:9092"
     val topics = "price_data_part4"
     val topicsSet = topics.split(",").toSet
 
@@ -29,7 +29,7 @@ val scCas = new SparkContext(confSparkCassandra)
     // Create direct kafka stream with brokers and topics
     val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
-
+    val divi:Int=5
     messages.foreachRDD { rdd =>
 
         val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
@@ -37,13 +37,27 @@ val scCas = new SparkContext(confSparkCassandra)
 	import sqlContext.implicits._
         val lines = rdd.map(_._2)
 	val df = sqlContext.jsonRDD(lines)
-	var df2=df.select(df("location"),df("item"),df("time"))
-  	val df3=df2.map{case Row(x:String,y:String,z:Long)=>((x,y),(z,z))}	
-	val df4=df3.reduceByKey((x,y)=>(math.min(x._1,y._1),math.max(x._2,y._2)))
-	val df5=df4.map{case ((a,b),(c,d))=>(a,b,d,c)}
+	var df2=df.select(df("location"),df("item"),df("time")).sort("location","item","time")
+	//df2.show(30)
+  	val df3=df2.map{case Row(x:String,y:String,z:Long)=>((x,y),(z,z,z))}	
+	val df4=df3.reduceByKey((x,y)=>(math.min(x._1,y._1),math.max(x._2,y._2),x._3))
+	val df5=df4.map{case ((a,b),(c,d,e))=>(a,b,d,c)}
+	val df6=df4.map{case ((a,b),(c,d,e))=>((a,b),(d,c))}
+  	val df7=df2.map{case Row(x:String,y:String,z:Long)=>((x,y),z)}	
+
+	//val mandf3=df3.keyBy(t=>(t._1,t._2))
+	//val mandf4=df4.keyBy(t=>(t._1,t._2))
+	val jdf=df7.join(df6)
+	val jdf2=jdf.sortBy(x=>(x._1,x._2._1))
 	df5.saveToCassandra("play","tempdata")
-	//println(df4.toDF().first)
-	df5.toDF().show()
+	val test=jdf2.toDF().take(5)
+	//println(jdf.toDF().take(2))
+	println(test(0))
+	println(test(1))
+	println(test(2))
+	println(test(3))
+	println(test(4)) 
+	jdf2.toDF().show()
 	println(df4.asInstanceOf[AnyRef].getClass.getSimpleName)
 	df2.map{case Row(location: String,item: String,time:Long)=>dumbHere(location,item,time)}.saveToCassandra("play","dumbdata")
 	//df2.show(); 
