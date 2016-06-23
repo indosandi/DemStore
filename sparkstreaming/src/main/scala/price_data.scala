f6a3dd356b11b9a999bf6756cca395cbd61c84bf
Ubuntu <ubuntu@ip-172-31-1-71.us-west-2.compute.internal>
2016-06-23 18:18:27 +0000

    cleaning up scala

--- sparkstreaming/src/main/scala/price_data.scala
+++ sparkstreaming/src/main/scala/price_data.scala
@@ -16,13 +16,13 @@ import com.datastax.driver.core.utils._
 object JSONDataStreaming {
   def main(args: Array[String]) {
 
-    val brokers = "52.39.57.55:9092,52.40.218.111:9092,52.24.38.215:9092,52.33.31.202:9092"
-    val topics = "price_data_part4"
+    val brokers = "50.112.40.243:9092,52.25.13.29:9092,50.112.22.187:9092,52.24.80.162:9092" 
+    val topics = "price"
     val topicsSet = topics.split(",").toSet
 
     val confSparkCassandra  = new SparkConf().setAppName("price_data").set("spark.cassandra.connection.host", "52.39.96.29")
 val scCas = new SparkContext(confSparkCassandra)
-    val ssc = new StreamingContext(scCas, Seconds(10))
+    val ssc = new StreamingContext(scCas, Seconds(5))
     val rootLogger = Logger.getRootLogger()
     rootLogger.setLevel(Level.ERROR)
 
@@ -32,7 +32,6 @@ val scCas = new SparkContext(confSparkCassandra)
     val timeSpan:Long=10000
     val divi:Long=10
     val intv:Long=timeSpan/divi
-    println(intv)
     messages.foreachRDD { rdd =>
 
         val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
@@ -40,8 +39,11 @@ val scCas = new SparkContext(confSparkCassandra)
 	import sqlContext.implicits._
         val lines = rdd.map(_._2)
 	val df = sqlContext.jsonRDD(lines)
+
+
+	// functional programming for calculating accumulative distribution
+	// many variable expression is declared so that easier to be understood
 	var df2=df.select(df("location"),df("item"),df("time")).sort("location","item","time")
-	//df2.show(3)
   	val df3=df2.map{case Row(x:String,y:String,z:Long)=>((x,y),(z,z,z))}	
 	val df4=df3.reduceByKey((x,y)=>(math.min(x._1,y._1),math.max(x._2,y._2),x._3))
 	val df6=df4.map{case ((a,b),(c,d,e))=>((a,b),(d,c))}
@@ -50,35 +52,24 @@ val scCas = new SparkContext(confSparkCassandra)
 	val jdf2=jdf.sortBy(x=>(x._1,x._2._1))
 	val jdf3=jdf2
 	.map(x=>(x._1,(x._2._1,x._2._2._1,x._2._2._2,(x._2._1-x._2._2._2)*divi/(x._2._2._1-x._2._2._2))))
-	//.map(x=>(x._1,List(x._2._4)))
-	//.reduceByKey(_++_).map(x=>(x._1,x._2.groupBy(identity).toList.sortBy(_._1)))
-	//.map(x=>(x._1,x._2.map(k=>k._2).map(p=>p.size))) 
-	//.map(x=>(x._1,x._2.foldLeft(List[Int](0))((x,y)=>x:+(y+x.last))))
-
 	val jdf4=jdf3.map(x=>(x._1,List(x._2._4)))
 	val jdf5=jdf4.reduceByKey(_++_)
 	val jdf6=jdf5.map(x=>(x._1,x._2.groupBy(identity).toList.sortBy(_._1)))
 	val jdf7=jdf6.map(x=>(x._1,x._2.map(k=>k._2).map(p=>p.size))) 
+	// jdf8 is accumulative distribution groupped by key (location,item)
 	val jdf8=jdf7.map(x=>(x._1,x._2.foldLeft(List[Int](0))((x,y)=>x:+(y+x.last))))
-	//val jdf6=jdf5.map(x=>(x._1,x._2.map(p=>x._2.count(_==p))))
-	//df5.saveToCassandra("play","tempdata")
-	val test=jdf8.toDF().take(5)
-	//println(jdf.toDF().take(2))
-	println(test(0))
-	println(test(1))
-	println(test(2))
-	println(test(3))
-	println(test(4)) 
-		
-	//jdf7.toDF().show()
-	//println(df4.asInstanceOf[AnyRef].getClass.getSimpleName)
-	df2.map{case Row(location: String,item: String,time:Long)=>dumbHere(location,item,time)}.saveToCassandra("play","dumbdata")
-	//df2.show(); 
+
+	//temporary solution only
+	val jdf9=jdf8.map(x=>(x._1._1,x._1._2,x._2(0),x._2(1),x._2(2),x._2(3),x._2(4),x._2(5),x._2(6),x._2(7),x._2(8),x._2(9),x._2(10)))
+	jdf9.saveToCassandra("play","disti")
+	
+	//dumping data is not performed on demo to save space in database
+	//df2.map{case Row(location: String,item: String,time:Long)=>dumbHere(location,item,time)}.saveToCassandra("play","dumbdata")
+
+	// counting data
 	val dfsort=df.groupBy("location","item").count().sort("location","count")
 	val dfsortcount=dfsort.select(dfsort("location"),dfsort("item"),dfsort("count").cast("int"))
-	//dfsortcount.show(5)
 	dfsortcount.map{case Row(location: String,item: String,count: Int)=>countRT(location,item,count)}.saveToCassandra("play","sdata")
-	//println(lines.asInstanceOf[AnyRef].getClass.getSimpleName)
 	//println("--------------------------------------------")
     }
 
@@ -89,10 +80,8 @@ val scCas = new SparkContext(confSparkCassandra)
 }
 
 
-case class disti(location: String,item: String,min: Long, max:Long)
-case class minMax(location: String,item: String,min: Long, max:Long)
+//temporary solution
 case class countRT(location: String,item: String,count: Int)
-case class dumbHere(location: String,item: String,time:Long)
 
 /** Lazily instantiated singleton instance of SQLContext */
 object SQLContextSingleton {
